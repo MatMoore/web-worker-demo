4ff43f995cbccfd93bd75ec2383c5fd289f1a45f
Mat Moore <MatMoore@users.noreply.github.com>
2020-07-31 13:47:47 +0100

    forgot to commit this
    ------------------------------------------------------------------
    
        /\_____/\
       /  o   o  \
      ( ==  ^  == )
       )         (
      (           )
     ( (  )   (  ) )
    (__(__)___(__)__)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert('hello')
+var myWorker = new Worker('worker.js');
+
+const messageInput = document.querySelector('#message');
+const messageForm = document.querySelector('#form');
+const responseDialog = document.querySelector('#response');
+
+form.onsubmit = function() {
+    myWorker.postMessage(messageInput.value);
+    console.log('Message posted to worker');
+    return false;
+}
+
+myWorker.onmessage = function(e) {
+    responseDialog.textContent = e.data;
+    console.log('Message received from worker');
+}
